0ea942350cc3e08c96fb2aafe865b955123c0dde
unknown <2086855608@qq.com>
2020-12-15 18:57:44 +0800

    lyq

---
+++ lyq/text121006/app/src/main/java/com/example/text121006/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.text121006;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.button:
+                        ProgressDialog progressDialog =new ProgressDialog(MainActivity.this);
+                        progressDialog.setTitle("123");
+                        progressDialog.setMessage("loadinding....");
+                        progressDialog.setCancelable(true);
+                        progressDialog.show();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+}
